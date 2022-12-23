d71700850db9cfa6db9f2421664fd47f37a84ce2
Ezh+Wine <valvic90@gmail.com>
2022-12-23 13:14:08 +0300

    luxfashion edited

---
+++ articles.js
@@ -0,0 +1,66 @@
+const articles =
+    `
+    [
+        {
+            "article_pic": "img/1st_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/2nd_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/3rd_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/4th_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/5th_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/6th_item.png",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/greydress.jpg",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Grey dress",
+            "article_price": "$52.00"
+
+        },
+        {
+            "article_pic": "img/dress.jpg",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Red dress blahblahblah",
+            "article_price": "$75.00"
+        },
+        {
+            "article_pic": "img/blackdress.jpg",
+            "article_title": "ellery x m'o capsule",
+            "article_desc": "Black dress",
+            "article_price": "$100.00"
+        }
+    ]
+`
